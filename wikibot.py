759ab7e5e9fde5dc549f46a0bfdeb6bc474ce50b
dubitable <pierrequereuil@gmail.com>
2021-03-11 11:41:30 -0800

    Added the "getarticle" function to retrieve a brief description of it

--- wikibot.py
+++ wikibot.py
@@ -1,5 +1,6 @@
-import tweepy, json, requests, io, os
+import tweepy, json, requests, io, os, re
 from PIL import Image
+from bs4 import BeautifulSoup
 
 class wikibot:
     def __init__(self, startpath):
@@ -9,7 +10,7 @@ class wikibot:
         auth = tweepy.OAuthHandler(keys["consumerkey"], keys["consumerkeysecret"])
         auth.set_access_token(keys["accesstoken"], keys["accesstokensecret"])
 
-        self.article = startpath 
+        self.articleurl = startpath 
         self.api = tweepy.API(auth)
 
     def deleteimages(self):
@@ -24,11 +25,26 @@ class wikibot:
         media = self.api.media_upload(os.path.join("images",os.listdir("images")[0]))
         return media
     
+    def format(self, elem):
+        text = elem.text.strip().replace("(listen)","")
+        text = re.sub(r"\[(.*?)\]|\((.*?)\)", "", text)
+        text = text.replace("  ", " ")
+        return re.match(r"(.*?\.)", text).groups()[0]
+
+    def getarticle(self, articleurl = None):
+        if articleurl is None: articleurl = self.articleurl
+        page = requests.get(articleurl).content
+        soup = BeautifulSoup(page,"html.parser")
+        for elem in soup.find_all("p")[0:4]:
+            try: elem["class"]
+            except: break
+        text = self.format(elem)
+        return text
+
     def tweet(self, message, imageurl):
         media = self.uploadimage(imageurl)
         media = media.media_id_string
         self.api.update_status(status = message, media_ids=[media])
 
-bot = wikibot("idk")
-url = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Gurbanguly_Berdimuhamedow_%282017-10-02%29_02.jpg/220px-Gurbanguly_Berdimuhamedow_%282017-10-02%29_02.jpg"
-bot.tweet("testing",url)
+bot = wikibot("https://en.wikipedia.org/wiki/Wikipedia")
+print(bot.getarticle())
