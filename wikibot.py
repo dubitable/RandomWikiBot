6c6423686c5eee5cee8dd9f76e3949e94c58581f
dubitable <pierrequereuil@gmail.com>
2021-03-12 18:55:42 -0800

    Cleaned up code

--- wikibot.py
+++ wikibot.py
@@ -4,13 +4,16 @@ from bs4 import BeautifulSoup
 from profanity import profanity
 
 class wikibot:
-    def __init__(self):
+    conditionlist = [":", "Main_Page", "List", "("]
+
+    def __init__(self, articleurl=""):
         with open("keys.json","r") as file:
             keys = json.loads(file.read())
 
         auth = tweepy.OAuthHandler(keys["consumerkey"], keys["consumerkeysecret"])
         auth.set_access_token(keys["accesstoken"], keys["accesstokensecret"])
         self.api = tweepy.API(auth)
+        self.startpath = articleurl
 
     def deleteimages(self):
         for elem in os.listdir("images"):
@@ -36,15 +39,14 @@ class wikibot:
             x+=1
         return text
 
-    def getarticle(self, articleurl):
-        page = requests.get(articleurl).content
-        soup = BeautifulSoup(page,"html.parser")
-        for elem in soup.find_all("p")[0:4]:
-            try: elem["class"]
-            except: break
-        text = self.format(elem)
-        return text
-    
+    def conditions(self, text):
+        if text.startswith("/wiki/") and not profanity.contains_profanity(text):
+            for elem in self.conditionlist:
+                if elem in text:
+                    return False
+            return True
+        return False
+
     def findnewurl(self, url):
         page = requests.get(url).content
         soup = BeautifulSoup(page,"html.parser")
@@ -52,37 +54,59 @@ class wikibot:
         for elem in soup.find_all("a"):
             try: text = elem["href"]
             except: continue
-            if text.startswith("/wiki/") and ":" not in text and "Main_Page" not in text and "(" not in text and not profanity.contains_profanity(text):
+            if self.conditions(text):
                 links.append(text)
-        return random.choice(links)
+        return random.choice(links), soup
+
+    def getarticle(self, articleurl, soup=None):
+        if soup is None:
+            page = requests.get(articleurl).content
+            soup = BeautifulSoup(page,"html.parser")
+        for elem in soup.find_all("p"):
+            try: elem["class"]
+            except: break
+        text = self.format(elem)
+        return text
     
-    def getimageurl(self, url):
-        page = requests.get(url).content
-        soup = BeautifulSoup(page,"html.parser")
+    def getimageurl(self, url, soup=None):
+        if soup is None:
+            page = requests.get(url).content
+            soup = BeautifulSoup(page,"html.parser")
         for image in soup.find_all("img"):
             if "//upload.wikimedia.org/wikipedia/commons/thumb/" in image["src"]:
                 return "https:" + image["src"]
         return None
 
-    def getelements(self, url):
+    def getelements(self, url, first = False):
         text = None
         while text is None or len(text) > 280 or imageurl is None:
-            possibleurl = "https://www.wikipedia.org"+self.findnewurl(url)
-            text = self.getarticle(possibleurl)
-            imageurl = self.getimageurl(possibleurl)
+            possibleurl = self.startpath
+            if not first: possibleurl, soup = self.findnewurl(url)
+            possibleurl = "https://www.wikipedia.org"+ possibleurl
+            text = self.getarticle(possibleurl, soup)
+            imageurl = self.getimageurl(possibleurl, soup)
         self.getimageurl = possibleurl
         with open("links.txt", "a") as file:
-            file.write("\n"+possibleurl)
+            file.write(possibleurl + "\n")
         return text, possibleurl, imageurl
-
+    
+    def reset(self):
+        with open("links.txt","w") as file:
+            file.write("")
+        
     def tweet(self):
         with open("links.txt", "r") as file:
-            self.articleurl = file.readlines()[-1]
-        message, url, imageurl = bot.getelements(self.articleurl) 
-        message += "\n" + url
+            try:
+                lastline = file.readlines()[-1]
+                articleurl = lastline
+            except: 
+                if self.startpath == "": raise ValueError("You must enter a path for the starting point in the constructor.")
+                articleurl = self.startpath
+        message, url, imageurl = bot.getelements(articleurl) 
+        message += "\n" + articleurl
         media = self.uploadimage(imageurl)
         media = media.media_id_string
         self.api.update_status(status = message, media_ids=[media])
 
-bot = wikibot()
+bot = wikibot("https://wikipedia.org/wiki/Wikipedia")
 bot.tweet()
